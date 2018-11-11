96b2dd34d2a54d79ae546c8c91b228a21bce04ec
jeafleohj <jhony.angulo@utec.edu.pe>
2018-11-10 19:19:04 -0500

    Matrix testing function set

--- matrix.hpp
+++ matrix.hpp
@@ -33,37 +33,81 @@ public:
 };
 
 template<typename T>
-void Matrix<T>::set(int x, int y, T data){
-	
+void Matrix<T>::set(int r, int c, T data){
+	if( -1 < c && c < matrix.cols &&
+		-1 < r && r < matrix.rows){
+		NodeValue<T>* nnode = new NodeValue<T>(c,r,data);
+		if( (*matrix.hColumns)[c]->nextValue == nullptr ){
+			(*matrix.hColumns)[c]->nextValue = nnode;
+		}else{
+			NodeValue<T>* it = (*matrix.hColumns)[c]->nextValue;
+			if( y < it->y ){
+				(*matrix.hRows)[x]->nextValue = nnode;
+				nnode->down = it;
+
+			}else{
+				while( it && it->y < y){
+					if(it->down == nullptr){
+						it->down = nnode;
+					}else if( it->down && y < it->down->y ){
+						nnode= it->down;
+						it->down = nnode;
+					}
+					it = it->down;
+				}				
+			}
+		}
+	}else{
+		throw std::out_of_range ("Index Error");
+	}
 }
+
 template<typename T>
 T Matrix<T>::operator()(int x, int y){
-
+	if( (*matrix.hColumns)[x]->nextValue != nullptr ){
+		NodeValue<T>* it = (*matrix.hColumns)[x]->nextValue;
+		while(it && it->y <= y){
+			if(it->y == y){
+				return it->data;
+			}
+			it = it->down;
+		}
+	}
+	return 0;
 }
+
 template<typename T>
 Matrix<T> Matrix<T>::operator*(Matrix<T> other){
 
 }
+
 template<typename T>
 Matrix<T> Matrix<T>::operator*(T scalar){
 
 }
+
 template<typename T>
 Matrix<T> Matrix<T>::operator+(Matrix<T> other){
 
 }
+
 template<typename T>
 Matrix<T> Matrix<T>::operator-(Matrix<T> other){
 
 }
-template<typename T>
 
+template<typename T>
 Matrix<T> Matrix<T>::transposed(){
 }
+
 template<typename T>
 Matrix<T> Matrix<T>::operator=(Matrix<T> other){
-
+	
 }
-~Matrix();
+
+template<typename T>
+Matrix<T>::~Matrix(){
+	
+}
 
 #endif
