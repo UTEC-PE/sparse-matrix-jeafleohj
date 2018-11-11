15da5cc5a2ea356601ad857da3960f895e77bbd1
jeafleohj <jhony.angulo@utec.edu.pe>
2018-11-10 19:18:42 -0500

    Nodes

--- node.hpp
+++ node.hpp
@@ -1,6 +1,7 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <iostream>
 using namespace std;
 
 template <typename T>
@@ -20,10 +21,16 @@ struct NodeLine{
 	NodeLine* nextNode;
 	NodeValue<T>* nextValue;
 	NodeLine(int p)
-		: pos(p)
-	{
-		nextValue = nullptr;
-		nextNode = nullptr;
+		: pos(p), nextValue(nullptr), nextNode(nullptr)
+	{}
+
+	NodeLine* operator[](int index){
+		NodeLine* tmp = this;
+		for(int i = 0; i<index; ++i) {
+			tmp = tmp->nextNode;
+		}
+		//cout << "asd" << "\n";
+		return tmp;
 	}
 };
 
@@ -36,17 +43,27 @@ struct NodeHeader{
 		hColumns = hRows = nullptr;
 		rows = cols = 0;
 	}
-	NodeHeader(int r, int c)
-		: rows(r), cols(c)
+	NodeHeader(int x, int y)
+		: rows(x), cols(y)
 	{
-		//hColumns = nullptr;
-		//	insertNode(hColumns);
+		hColumns = hRows = nullptr;
+		insertNodes(hRows, x);
+		insertNodes(hColumns, y);
 	}
 private:
-	void insertNode(NodeLine<T>* & nodo){
-		if(nodo == nullptr){
-			//nodo = new NodeLine<T>(0);
+	void insertNodes(NodeLine<T>* & nodo, int count){
+		for(int i = count-1; i>=0; i--) {
+			insertNode(nodo, i);
 		}
+	}
+	void insertNode(NodeLine<T>* & nodo, int pos=0){
+		NodeLine<T>* tmp = new NodeLine<T>(pos);
+		if(nodo == nullptr){
+			nodo = tmp;
+			return;
+		}
+		tmp->nextNode = nodo;
+		nodo = tmp;
 	}
 };
 
