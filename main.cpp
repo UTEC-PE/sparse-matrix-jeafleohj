03e576ad66760d9acfa369a15b79a892676dad09
jeafleohj <jhony.angulo@utec.edu.pe>
2018-11-11 21:55:06 -0500

    Testfile

--- main.cpp
+++ main.cpp
@@ -5,26 +5,23 @@ using namespace std;
 
 int main(){
 //int main(int argc, char *argv[]) {
-    Matrix<int> a(4, 7);
+    Matrix<int> a(7, 7), b;
 	a.set(0,5, 3);
 	a.set(0,6, 2);
 	a.set(0,0, 7);
-	
-	a.set(1,6, 2);
-	//a.set(2,5, 8);
-	//a.set(4,6, 1);
-	//a.set(5,5, 7);	
-	//a.set(3,6, 5);
-	//	a.set(2,5, 9);
-	//a.set(0,2, 1);
-	//a.set(2,3, 6);
-	//a.set(0,3, 8);
-	//a.set(5,6, -1);
-	//a.set(0,5, 5);
-
-	for(int i = 0; i<4;i++){//rows
+	a.set(0,1, 1);	
+	a.set(0,2, 1);
+	a.set(0,3, 8);
+	a.set(1,6, 7);
+	a.set(2,6, 8);
+	a.set(3,0,0);
+	//
+	b= a;
+	b = b * a.transposed();
+	cout << "RES: " << "\n";
+	for(int i = 0; i<7;i++){//rows
 		for(int j = 0; j<7; ++j) {//cols
-			cout << a(j,i) << " ";
+			cout << b(j,i) << " ";
 		}
 		cout << "\n";
 	}
